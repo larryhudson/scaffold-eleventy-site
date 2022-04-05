8d32a5d04802ffa99c81ccc29e46c576a783a180
Larry Hudson <larryhudson@hey.com>
2022-04-06 06:58:57 +1000

    update to suit sites instead of plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
-const { MyPlugin } = require("./index");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(MyPlugin);
-
   return {
     dir: {
-      input: "test_input",
-      output: "test_output",
+      input: "src",
+      output: "_site",
     },
   };
 };
